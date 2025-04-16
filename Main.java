cf51b97451cc3f19057dba00f8b039f437c3d0e2
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-16 15:58:50 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        // Print header for the Pokémon Battle Simulator
+        System.out.println("=== Pokémon Battle Simulator ===");
